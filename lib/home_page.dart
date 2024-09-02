5de506abb0c53f64ab7b225429ccc0f7a5bed06b
Aamir ali <aamiralisoomro73@gmail.com>
2024-09-01 20:59:58 -0700

    todo app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController nameController = TextEditingController();
-  List friendsNames = [""];
+  List friendsNames = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
